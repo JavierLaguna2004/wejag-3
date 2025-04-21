91974de792915a3287c9e46ef2e4adb5ac57c8ac
JavierLaguna2004 <154087942+JavierLaguna2004@users.noreply.github.com>
2025-04-20 18:52:12 -0600

    Nada

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    protected $table = 'Usuarios';
+    protected $table = 'usuarios';
     
     protected $primaryKey = 'id';
     
